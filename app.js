33ba39a1a2a5762230dfe676964d8c7c54eb682e
Ryan Frazier <frazierr2@gmail.com>
2020-11-24 22:00:07 -0500

    adding a few comments to understand code

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-// Getting user input from command line using 'process.argv'
+// GETTING USER SUPPLIED LOCATION THROUGH COMMAND LINE WITH 'process.argv'
 const location = process.argv[2]
+
+//  CHECK IF USER ENTERED LOCATION, IF THEY DO THEN RUN THE APP.
 if (!location) {
   console.log('Please provide a address');
 } else {
@@ -16,7 +18,6 @@ if (!location) {
       if (error) {
         return console.log(error);
       }
-
       console.log(geocodeData.location);
       console.log(forecastData);
     })
--- utils/forecast.js
+++ utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
     if (error) {
       callback('Could not connect to weather service', undefined)
     } else if (response.body.error) {
-      callback('Unable to find location', undefined)
+      callback('Unable to find location within Weather API', undefined)
     } else {
       callback(undefined, `It is currently ${response.body.current.temperature} degrees outside.`)
     }
